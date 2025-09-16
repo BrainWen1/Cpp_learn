60e0820c530c0493ef3ce3a6c15f5bb19df9d4de
BrainWen <2229553982@qq.com>
2025-09-16 11:34:48 +0800

    9.16 类简介：Sales_item 类，初识成员函数

--- chapter_1/test_sales_item/test.cpp
+++ chapter_1/test_sales_item/test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "sales_item.h"
+#include "Sales_item.h"
 
 int main(void) {
 
@@ -11,12 +11,13 @@ int main(void) {
 
         std::cout << book << std::endl;
     }
+    std::cout << std::endl;
 
     // 1.21
     Sales_item item1, item2;
     std::cin >> item1 >> item2;
 
-    std::cout << item1 + item2 << std::endl;
+    std::cout << item1 + item2 << std::endl << std::endl;
 
     // 1.22
     Sales_item items, item;
@@ -26,7 +27,27 @@ int main(void) {
 
         items += item;
     }
-    std::cout << items << std::endl;
+    std::cout << items << std::endl << std::endl;
+
+    // 1.5.2
+    // 1.23
+    int cnt = 1;
+    std::cin >> items;
+    while(std::cin >> item) {
+
+        if(items.isbn == item.isbn) {
+            items += item;
+            cnt++;
+        } else {
+            std::cout << items.isbn << " occurs " << cnt << " times" << std::endl;
+            std::cout << items << std::endl;
+
+            cnt = 1;
+            items = item;
+        }
+    }
+    std::cout << items.isbn << " occurs " << cnt << " times" << std::endl;
+    std::cout << items << std::endl << std::endl;
 
     return 0;
 }
