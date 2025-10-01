54968125492612b2fcdda61138c1e3042ef61a27
BrainWen <2229553982@qq.com>
2025-10-01 17:07:27 +0800

    10.1 类型转换  CMakeLists中添加更多配置

--- chapter_4/4_11.cpp
+++ chapter_4/4_11.cpp
@@ -8,9 +8,24 @@ using std::endl;
 int main(void) {
 
     // 显式转换
+    // 命名的强制类型转换
+    // static_cast
     int i = 4, j = 3;
     double d = static_cast<double> (i) / j;
     cout << d << endl;
+
+    void *p = &d;
+    double *pd = static_cast<double*> (p);
+
+    // const_cast
+    int *const pi = &i;
+    int *pii = const_cast<int*> (pi);
+
+    // reinterpret_cast
+    char *pc = reinterpret_cast<char*> (pii);
+
+    // 旧式的强制类型转换
+    char *pcc = (char*)pi; // 等价于reinterpret_cast
 
     return 0;
 }
