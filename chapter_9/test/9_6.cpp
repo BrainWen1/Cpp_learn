a9f300ae9504410a7b16092c3fb747b6d0fbb75f
BrainWen <2229553982@qq.com>
2025-10-20 22:55:55 +0800

    10.20 容器适配器

---
+++ chapter_9/test/9_6.cpp
@@ -0,0 +1,13 @@
+// 容器适配器
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+
+int main(void) {
+    std::queue<int> q;;
+    std::priority_queue<int> pq;
+    std::stack<int> st;
+
+    return 0;
+}
